0f84ce02709b5dec34dc44ceb0849e3503de940b
Ilya-via <ilya95v@gmail.com>
2021-12-13 23:08:06 +0300

    review

--- js/main.js
+++ js/main.js
@@ -1,28 +1,3 @@
-// let element = document.querySelectorAll('.button_online')[0];
-// element.addEventListener("mousedown", function() {
-//   element.classList.add('button_online--push')
-// });
-// element.addEventListener("mouseup", function() {
-//   element.classList.remove('button_online--push')
-// });
-// element.addEventListener("mouseout", function() {
-//   element.classList.remove('button_online--push')
-// });
-
-
-// let knopka = document.querySelectorAll('.button_shell')[0];
-// knopka.addEventListener("mousedown", function() {
-//   knopka.classList.add('button_shell--push')
-// });
-// knopka.addEventListener("mouseup", function() {
-//   knopka.classList.remove('button_shell--push')
-// });
-// knopka.addEventListener("mouseout", function() {
-//   knopka.classList.remove('button_shell--push')
-// });
-
-
-
 function buttonHoldChangeStyle(el, btn_style_class) {
 	el.addEventListener("mousedown", function () {
 		el.classList.add(btn_style_class);
@@ -36,12 +11,21 @@ function buttonHoldChangeStyle(el, btn_style_class) {
 	});
 }
 
-document.querySelectorAll('.button_online').forEach(el_mass => {buttonHoldChangeStyle(el_mass, 'button_online--push')} );
-document.querySelectorAll('.button_shell').forEach(el_mass => {buttonHoldChangeStyle(el_mass, 'button_shell--push')} );
-
-document.querySelectorAll('.button_left_examples').forEach( function (element) {
-	element.addEventListener("dblclick", function (event) {
-	event.preventDefault();
-			console.log('hh');
+document.querySelectorAll('.button_online').forEach(el_mass => {
+	buttonHoldChangeStyle(el_mass, 'button_online--push')
 });
+document.querySelectorAll('.button_shell').forEach(el_mass => {
+	buttonHoldChangeStyle(el_mass, 'button_shell--push')
+});
+
+document.querySelectorAll('.button_left_examples').forEach(function (element) {
+	element.addEventListener("dblclick", function (event) {
+		event.preventDefault();
+	});
+});
+
+document.querySelectorAll('a[href="#"]').forEach(function (element) {
+	element.addEventListener("click", function (event) {
+		event.preventDefault();
+	});
 });
